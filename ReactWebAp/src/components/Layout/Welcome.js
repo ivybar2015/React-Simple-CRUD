d0bc380f0677edad5751541abcf2103f93e01e7a
ivybar2015 <camnhung_185@yahoo.com>
2020-09-29 14:55:38 -0700

    CRUD

--- ReactWebAp/src/components/Layout/Welcome.js
+++ ReactWebAp/src/components/Layout/Welcome.js
@@ -1,12 +1,16 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { Component } from "react";
+import "./layout.css";
+
 
 import pic from "./Pictures/welcomepage.jpg";
 
 function Wecome() {
     return (
-        <div className="welcome-pic">
-            <img src={pic} height="40%" width="70%" alt="picture" />
+        // eslint-disable-next-line react/jsx-no-comment-textnodes
+        <div >
+            <img src={pic} className="floatLeftIvyStyle" />
         </div>
     );
 }
